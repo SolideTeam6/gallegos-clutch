efd92c30bb8b19fce958aada5ba4348bf9056628
alexise26 <alexise26@gmail.com>
2016-10-13 22:21:35 -0600

    Third commit

--- js/loader.js
+++ js/loader.js
@@ -11,7 +11,7 @@ $(function () {
     $("#btn-servicios").click(function (e) {
         e.preventDefault();
         $('html,body').animate({
-            scrollTop: $("#taller").offset().top
+            scrollTop: $("#taller").offset().top -70
         }, 2000);
     });
 });
@@ -23,7 +23,186 @@ $(function () {
         }, 2000);
     });
 });
+$(function () {
+    $("#scroller").click(function (e) {
+        e.preventDefault();
+        $('html,body').animate({ scrollTop: $("#cinturon").offset().top - 70}, 2000);
+    });
+});
+$(function () {
+    $("#btn-car").click(function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: $("#taller").offset().top - 70
+        }, 2000);
+    });
+});
+$(function () {
+    $("#btn-car2").click(function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: $("#taller").offset().top -70
+        }, 2000);
+    });
+});
+$(function () {
+    $("#btn-car3").click(function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: $("#taller").offset().top -70
+        }, 2000);
+    });
+});
+$(function () {
+    $("#ver-mapa").click(function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: $("#map").offset().top -70
+        }, 2000);
+    });
+});
 
+$(function($) { 
+        $('#contactanos').waypoint(function() {
+            $(this).toggleClass( 'bounceInLeft animated' );
+            $("#contactanos").css("visibility", "visible");
+        },
+        {
+            offset: '70%',
+            triggerOnce: true
+
+        });
+        $('#map').waypoint(function() {
+            $(this).toggleClass( 'fadeInLeft animated' );
+            $("#map").css("visibility", "visible");
+        },
+        {
+            offset: '70%',
+            triggerOnce: true
+
+        });
+
+        $('#small-cont').waypoint(function() {
+            $(this).toggleClass( 'fadeInRight animated' );
+            $("#small-cont").css("visibility", "visible");
+        },
+        {
+            offset: '70%',
+            triggerOnce: true
+
+        });
+        $('.circulo-rs').waypoint(function() {
+            $(this).toggleClass( 'rubberBand animated' );
+        },
+        {
+            offset: '70%',
+            triggerOnce: true
+
+        });
+        $('#fade-ser').waypoint(function() {
+            $(this).toggleClass( 'fadeIn animated' );
+            $("#fade-ser").css("visibility", "visible");
+        },
+        {
+            offset: '70%',
+            triggerOnce: true
+
+        });
+        $('#piezas').waypoint(function() {
+            $(this).toggleClass( 'fadeInRight animated' );
+            $("#piezas").css("visibility", "visible");
+        },
+        {
+            offset: '70%',
+            triggerOnce: true
+
+        });
+        $('#horario').waypoint(function() {
+            $(this).toggleClass( 'fadeInLeft animated' );
+            $("#horario").css("visibility", "visible");
+        },
+        {
+            offset: '70%',
+            triggerOnce: true
+
+        });
+        $('#taller2').waypoint(function() {
+            $(this).toggleClass( 'fadeInLeft animated' );
+            $("#taller2").css("visibility", "visible");
+        },
+        {
+            offset: '70%',
+            triggerOnce: true
+
+        });
+        $('#imagenes').waypoint(function() {
+            $(this).toggleClass( 'fadeIn animated' );
+            $("#imagenes").css("visibility", "visible");
+        },
+        {
+            offset: '70%',
+            triggerOnce: true
+
+        });
+        $('#acerca-de').waypoint(function() {
+            $(this).toggleClass( 'fadeIn animated' );
+            $("#acerca-de").css("visibility", "visible");
+        },
+        {
+            offset: '70%',
+            triggerOnce: true
+
+        });
+        $('#mision').waypoint(function() {
+            $(this).toggleClass( 'fadeInLeft animated' );
+            $("#mision").css("visibility", "visible");
+        },
+        {
+            offset: '70%',
+            triggerOnce: true
+
+        });
+        $('#vision').waypoint(function() {
+            $(this).toggleClass( 'fadeInRight animated' );
+            $("#vision").css("visibility", "visible");
+        },
+        {
+            offset: '70%',
+            triggerOnce: true
+
+        });
+        $('.img-fade-gal').waypoint(function() {
+            $(this).toggleClass( 'flipInY animated' );
+            $(".img-fade-gal").css("visibility", "visible");
+        },
+        {
+            offset: '70%',
+            triggerOnce: true
+
+        });
+        $('#log-foo').waypoint(function() {
+            $('#bottom-love').toggleClass( 'fadeInUp animated' );
+            $("#bottom-love").css("visibility", "visible");
+        },
+        {
+            offset: '70%',
+            triggerOnce: true
+
+        });
+        $('#log-foo').waypoint(function() {
+            $('#bottom-solide').toggleClass( 'fadeInUp animated' );
+            $("#bottom-solide").css("visibility", "visible");
+        },
+        {
+            offset: '70%',
+            triggerOnce: true
+
+        });
+        
+       
+        
+        
+    });
 document.onkeypress = function (event) {
         event = (event || window.event);
         if (event.keyCode == 123) {
@@ -31,3 +210,24 @@ document.onkeypress = function (event) {
             return false;
         }
     }
+
+$(function($) {  
+    $('.circulo-rs').waypoint(function() {
+            $('#bottom-love').toggleClass( 'fadeInUp animated' );
+            $("#bottom-love").css("visibility", "visible");
+        },
+        {
+            offset: '70%',
+            triggerOnce: true
+
+        });
+        $('.circulo-rs').waypoint(function() {
+            $('#bottom-solide').toggleClass( 'fadeInUp animated' );
+            $("#bottom-solide").css("visibility", "visible");
+        },
+        {
+            offset: '70%',
+            triggerOnce: true
+
+        });
+});  
